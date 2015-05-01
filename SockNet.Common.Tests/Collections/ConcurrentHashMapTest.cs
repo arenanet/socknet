2c553800b6a89c21b41c5a2d3f5ddd231d6c25b7
Elvir Bahtijaragic <elvir@arena.net>
2015-05-01 12:20:06 -0700

    Adding simple concurrent hash map implementation - to eventually get around the fact that .NET 2.0 doesn’t have concurrent collections.

---
+++ SockNet.Common.Tests/Collections/ConcurrentHashMapTest.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArenaNet.SockNet.Common.Collections
+{
+    [TestClass]
+    public class ConcurrentHashMapTest
+    {
+        [TestMethod]
+        public void TestSingleThreaded()
+        {
+            TestDictionaryWithSingleThread(new Dictionary<string, string>(), 10000);
+            TestDictionaryWithSingleThread(new ConcurrentDictionary<string, string>(), 10000);
+            TestDictionaryWithSingleThread(new ConcurrentHashMap<string, string>(1024, 128), 10000);
+        }
+
+        [TestMethod]
+        public void TestMultiThreaded()
+        {
+            TestDictionaryWithMultipleThreads(new ConcurrentDictionary<string, string>(), 10000);
+            TestDictionaryWithMultipleThreads(new ConcurrentHashMap<string, string>(1024, 128), 10000);
+        }
+
+        private static void TestDictionaryWithSingleThread(IDictionary<string, string> dictionary, int size)
+        {
+            GC.Collect();
+            GC.WaitForFullGCComplete();
+            GC.WaitForPendingFinalizers();
+
+            ///>- PUT -<///
+            DateTime startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                dictionary["testKey" + i] = "testValue" + i;
+            }
+
+            TimeSpan timeTook = DateTime.Now.Subtract(startTime);
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " puts took: {0:g}", timeTook);
+
+            Assert.AreEqual(size, dictionary.Count);
+
+            string[] getValues = new string[size];
+
+            ///>- GET -<///
+            startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                dictionary.TryGetValue("testKey" + i, out getValues[i]);
+            }
+
+            timeTook = DateTime.Now.Subtract(startTime);
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " gets took: {0:g}", timeTook);
+
+            Assert.AreEqual(size, dictionary.Count);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual("testValue" + i, getValues[i]);
+            }
+
+            ///>- UPDATE -<///
+            startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                dictionary["testKey" + i] = "testValue" + (size - i);
+            }
+
+            timeTook = DateTime.Now.Subtract(startTime);
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " updates took: {0:g}", timeTook);
+
+            Assert.AreEqual(size, dictionary.Count);
+
+            ///>- REMOVE -<///
+            startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                dictionary.Remove("testKey" + i);
+            }
+
+            timeTook = DateTime.Now.Subtract(startTime);
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " removes took: {0:g}", timeTook);
+
+            Assert.AreEqual(0, dictionary.Count);
+
+            Console.WriteLine();
+        }
+
+        private static void TestDictionaryWithMultipleThreads(IDictionary<string, string> dictionary, int size)
+        {
+            GC.Collect();
+            GC.WaitForFullGCComplete();
+            GC.WaitForPendingFinalizers();
+
+            ///>- PUT -<///
+            DateTime startTime = DateTime.Now;
+
+            int completedCount = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                ThreadPool.QueueUserWorkItem((object state) => 
+                { 
+                    dictionary["testKey" + (int)state] = "testValue" + (int)state;
+                    Interlocked.Increment(ref completedCount);
+                }, i);
+            }
+
+            TimeSpan timeTook;
+
+            while ((timeTook = DateTime.Now.Subtract(startTime)).Seconds < 5 && completedCount < size)
+            {
+                // do stuff
+            }
+
+            Assert.AreEqual(size, dictionary.Count);
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " puts took: {0:g}", timeTook);
+
+            string[] getValues = new string[size];
+
+            ///>- GET -<///
+            completedCount = 0;
+            startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                ThreadPool.QueueUserWorkItem((object state) =>
+                {
+                    dictionary.TryGetValue("testKey" + (int)state, out getValues[(int)state]);
+                    Interlocked.Increment(ref completedCount);
+                }, i);
+            }
+
+            while ((timeTook = DateTime.Now.Subtract(startTime)).Seconds < 5 && completedCount < size)
+            {
+                // do stuff
+            }
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " gets took: {0:g}", timeTook);
+
+            Assert.AreEqual(size, dictionary.Count);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual("testValue" + i, getValues[i]);
+            }
+
+            ///>- UPDATE -<///
+            completedCount = 0;
+            startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                ThreadPool.QueueUserWorkItem((object state) =>
+                {
+                    dictionary["testKey" + (int)state] = "testValue" + (int)state;
+                    Interlocked.Increment(ref completedCount);
+                }, i);
+            }
+
+            while ((timeTook = DateTime.Now.Subtract(startTime)).Seconds < 5 && completedCount < size)
+            {
+                // do stuff
+            }
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " updates took: {0:g}", timeTook);
+
+            Assert.AreEqual(size, dictionary.Count);
+
+            ///>- REMOVE -<///
+            completedCount = 0;
+            startTime = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+            {
+                ThreadPool.QueueUserWorkItem((object state) =>
+                {
+                    dictionary.Remove("testKey" + (int)state);
+                    Interlocked.Increment(ref completedCount);
+                }, i);
+            }
+
+            while ((timeTook = DateTime.Now.Subtract(startTime)).Seconds < 5 && completedCount < size)
+            {
+                // do stuff
+            }
+
+            Console.WriteLine(dictionary.GetType().Name + ":\t" + size + " removes took: {0:g}", timeTook);
+
+            Assert.AreEqual(0, dictionary.Count);
+            
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SockNet.Common/Collections/ConcurrentHashMap.cs
@@ -0,0 +1,585 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ArenaNet.SockNet.Common.Collections
+{
+    /// <summary>
+    /// A concurrent hashmap. It uses splicing to lock buckets.
+    /// </summary>
+    /// <typeparam name="K">the key type</typeparam>
+    /// <typeparam name="V">the value type</typeparam>
+    public class ConcurrentHashMap<K, V> : IDictionary<K, V>
+    {
+        /// <summary>
+        /// The node implementation.
+        /// </summary>
+        internal class Node
+        {
+            internal KeyValuePair<K, V> kvp;
+            internal volatile Node next;
+
+            internal Node(KeyValuePair<K, V> kvp, Node next = null)
+            {
+                this.kvp = kvp;
+                this.next = next;
+            }
+        }
+
+        private Node[] lookupTable;
+        private object[] locks;
+        private int count;
+
+        /// <summary>
+        /// Returns the count (number of keyvaluepairs) in this hash map.
+        /// </summary>
+        public int Count
+        {
+            get { return count; }
+        }
+
+        /// <summary>
+        /// Always returns false.
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Create a concurrent hashmap that stripes access to the undelying buckets in the lookup table.
+        /// </summary>
+        /// <param name="lookupTableSize"></param>
+        /// <param name="lockCount"></param>
+        public ConcurrentHashMap(uint lookupTableSize = 128, uint lockCount = 16)
+        {
+            if (!IsPowerOfTwo(lookupTableSize))
+            {
+                throw new ArgumentException("'lookupTableSize' needs to be a power of two.");
+            }
+
+            if (!IsPowerOfTwo(lockCount))
+            {
+                throw new ArgumentException("''lockCount' needs to be a power of two.");
+            }
+
+            lookupTable = new Node[lookupTableSize];
+            locks = new object[lockCount];
+            for (int i = 0; i < locks.Length; i++)
+            {
+                locks[i] = new object();
+            }
+
+            count = 0;
+        }
+
+        /// <summary>
+        /// Returns all the keys in this hash map.
+        /// </summary>
+        public ICollection<K> Keys
+        {
+            get
+            {
+                List<K> keys = new List<K>();
+
+                for (int i = 0; i < lookupTable.Length; i++)
+                {
+                    lock (GetMutexFor(i))
+                    {
+                        Node currentNode = lookupTable[i];
+
+                        while (currentNode != null)
+                        {
+                            keys.Add(currentNode.kvp.Key);
+                            currentNode = currentNode.next;
+                        }
+                    }
+                }
+
+                return keys;
+            }
+        }
+
+        /// <summary>
+        /// Returns are the values in this hash map.
+        /// </summary>
+        public ICollection<V> Values
+        {
+            get
+            {
+                List<V> values = new List<V>();
+
+                for (int i = 0; i < lookupTable.Length; i++)
+                {
+                    lock (GetMutexFor(i))
+                    {
+                        Node currentNode = lookupTable[i];
+
+                        while (currentNode != null)
+                        {
+                            values.Add(currentNode.kvp.Value);
+                            currentNode = currentNode.next;
+                        }
+                    }
+                }
+
+                return values;
+            }
+        }
+
+        /// <summary>
+        /// Arrays accessors for this hash map.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public V this[K key]
+        {
+            get
+            {
+                V response;
+
+                if (TryGetValue(key, out response))
+                {
+                    return response;
+                }
+                else
+                {
+                    throw new KeyNotFoundException("No value found for key: " + key);
+                }
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Remove(key);
+                }
+                else
+                {
+                    Add(key, value);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Adds a keyvaluepair to this hasm map.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(KeyValuePair<K, V> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        /// <summary>
+        /// Adds a key and a value to this hash map.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(K key, V value)
+        {
+            int hash = Smear(key.GetHashCode());
+            int index = IndexFor(hash, lookupTable.Length);
+
+            lock (GetMutexFor(index))
+            {
+                Node foundNode = lookupTable[index];
+
+                if (foundNode == null)
+                {
+                    lookupTable[index] = new Node(new KeyValuePair<K, V>(key, value));
+                    Interlocked.Increment(ref count);
+                }
+                else
+                {
+                    Node lastNode = null;
+
+                    while (foundNode != null)
+                    {
+                        if (foundNode.kvp.Key.Equals(key))
+                        {
+                            break;
+                        }
+
+                        lastNode = foundNode;
+                        foundNode = foundNode.next;
+                    }
+
+                    if (foundNode != null)
+                    {
+                        foundNode.kvp = new KeyValuePair<K, V>(key, value);
+                    }
+                    else
+                    {
+                        lastNode.next = new Node(
+                            new KeyValuePair<K, V>(key, value),
+                            lastNode.next
+                        );
+                        Interlocked.Increment(ref count);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this hash map contains the given keyvaluepair.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(KeyValuePair<K, V> item)
+        {
+            V response;
+
+            if (TryGetValue(item.Key, out response))
+            {
+                return response.Equals(item.Value);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the given key exists.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool ContainsKey(K key)
+        {
+            int hash = Smear(key.GetHashCode());
+            int index = IndexFor(hash, lookupTable.Length);
+
+            lock (GetMutexFor(index))
+            {
+                Node foundNode = lookupTable[index];
+
+                if (foundNode == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    while (foundNode != null)
+                    {
+                        if (foundNode.kvp.Key.Equals(key))
+                        {
+                            break;
+                        }
+
+                        foundNode = foundNode.next;
+                    }
+
+                    if (foundNode != null)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the given key with the given value from this hash map.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(KeyValuePair<K, V> item)
+        {
+            return Remove(item.Key, item.Value);
+        }
+
+        /// <summary>
+        /// Removes the given key from this hash map.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Remove(K key)
+        {
+            int hash = Smear(key.GetHashCode());
+            int index = IndexFor(hash, lookupTable.Length);
+
+            lock (GetMutexFor(index))
+            {
+                Node foundNode = lookupTable[index];
+
+                if (foundNode == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    Node lastNode = null;
+
+                    while (foundNode != null)
+                    {
+                        if (foundNode.kvp.Key.Equals(key))
+                        {
+                            break;
+                        }
+
+                        lastNode = foundNode;
+                        foundNode = foundNode.next;
+                    }
+
+                    if (foundNode != null)
+                    {
+                        if (lastNode != null)
+                        {
+                            lastNode.next = foundNode.next;
+                        }
+                        else
+                        {
+                            lookupTable[index] = foundNode.next;
+                        }
+
+                        Interlocked.Decrement(ref count);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the given key with the given value from the hash map.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Remove(K key, V value)
+        {
+            int hash = Smear(key.GetHashCode());
+            int index = IndexFor(hash, lookupTable.Length);
+
+            lock (GetMutexFor(index))
+            {
+                Node foundNode = lookupTable[index];
+
+                if (foundNode == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    Node lastNode = null;
+
+                    while (foundNode != null)
+                    {
+                        if (foundNode.kvp.Key.Equals(key))
+                        {
+                            break;
+                        }
+
+                        lastNode = foundNode;
+                        foundNode = foundNode.next;
+                    }
+
+                    if (foundNode != null && foundNode.kvp.Value.Equals(value))
+                    {
+                        if (lastNode != null)
+                        {
+                            lastNode.next = foundNode.next;
+                        }
+                        else
+                        {
+                            lookupTable[index] = foundNode.next;
+                        }
+
+                        Interlocked.Decrement(ref count);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the value for the given key and returns true if found.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetValue(K key, out V value)
+        {
+            int hash = Smear(key.GetHashCode());
+            int index = IndexFor(hash, lookupTable.Length);
+
+            lock (GetMutexFor(index))
+            {
+                Node foundNode = lookupTable[index];
+
+                if (foundNode == null)
+                {
+                    value = default(V);
+                    return false;
+                }
+                else
+                {
+                    while (foundNode != null)
+                    {
+                        if (foundNode.kvp.Key.Equals(key))
+                        {
+                            break;
+                        }
+
+                        foundNode = foundNode.next;
+                    }
+
+                    if (foundNode != null)
+                    {
+                        value = foundNode.kvp.Value;
+                        return true;
+                    }
+                    else
+                    {
+                        value = default(V);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears all keybaluepairs from this hash map.
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < lookupTable.Length; i++)
+            {
+                lock (GetMutexFor(i))
+                {
+                    while (lookupTable[i] != null)
+                    {
+                        lookupTable[i] = lookupTable[i].next;
+                        Interlocked.Decrement(ref count);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copies the contents of this hash map into the given array.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
+        {
+            if (arrayIndex > array.Length)
+            {
+                return;
+            }
+
+            IEnumerator<KeyValuePair<K, V>> enumerator = GetEnumerator();
+
+            for (int i = arrayIndex; i < array.Length; i++)
+            {
+                if (enumerator.MoveNext())
+                {
+                    array[i] = enumerator.Current;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the enumerator for this hash map.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            List<KeyValuePair<K, V>> kvps = new List<KeyValuePair<K, V>>();
+
+            for (int i = 0; i < lookupTable.Length; i++)
+            {
+                lock (GetMutexFor(i))
+                {
+                    Node currentNode = lookupTable[i];
+
+                    while (currentNode != null)
+                    {
+                        kvps.Add(currentNode.kvp);
+                        currentNode = currentNode.next;
+                    }
+                }
+            }
+
+            return kvps.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the enumerator for this hash map.
+        /// </summary>
+        /// <returns></returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the mutex for the given index.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private object GetMutexFor(int index)
+        {
+            return locks[IndexFor(index, locks.Length)];
+        }
+
+        /// <summary>
+        /// Checks if the given number is a power of two.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private static bool IsPowerOfTwo(uint x)
+        {
+            return (x & (x - 1)) == 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hash"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private static int IndexFor(int hash, int length)
+        {
+            return Abs(hash) & (length - 1);
+        }
+
+        /// <summary>
+        /// Applies a supplemental hash function to a given hashCode, which
+        /// defends against poor quality hash functions.  This is critical
+        /// because HashMap uses power-of-two length hash tables, that
+        /// otherwise encounter collisions for hashCodes that do not differ
+        /// in lower bits. Note: Null keys always map to hash 0, thus index 0.
+        /// </summary>
+        /// <param name="hashCode"></param>
+        /// <returns></returns>
+        private static int Smear(int hashCode)
+        {
+            hashCode ^= (hashCode >> 20) ^ (hashCode >> 12);
+            return hashCode ^ (hashCode >> 7) ^ (hashCode >> 4);
+        }
+
+        /// <summary>
+        /// Gets the absolute value of the given integer.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private static int Abs(int x)
+        {
+            return (x + (x >> 31)) ^ (x >> 31);
+        }
+    }
+}
